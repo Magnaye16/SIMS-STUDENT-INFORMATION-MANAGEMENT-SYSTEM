ff2412695271d8dc45a1203d4471793f7247f32f
Magnaye16 <151270874+Magnaye16@users.noreply.github.com>
2024-10-15 20:20:10 +0800

    basic login

---
+++ LOGIN.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class LOGIN
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim CustomizableEdges9 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges10 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges1 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges2 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges3 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges4 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges5 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges6 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges7 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges8 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
+        Guna2GradientButton2 = New Guna.UI2.WinForms.Guna2GradientButton()
+        Guna2GradientButton1 = New Guna.UI2.WinForms.Guna2GradientButton()
+        Guna2HtmlLabel3 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Guna2HtmlLabel2 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        Guna2HtmlLabel1 = New Guna.UI2.WinForms.Guna2HtmlLabel()
+        studname_txtbx = New Guna.UI2.WinForms.Guna2TextBox()
+        uid_txtbx = New Guna.UI2.WinForms.Guna2TextBox()
+        Guna2Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Guna2Panel1
+        ' 
+        Guna2Panel1.BackColor = SystemColors.InfoText
+        Guna2Panel1.Controls.Add(Guna2GradientButton2)
+        Guna2Panel1.Controls.Add(Guna2GradientButton1)
+        Guna2Panel1.Controls.Add(Guna2HtmlLabel3)
+        Guna2Panel1.Controls.Add(Guna2HtmlLabel2)
+        Guna2Panel1.Controls.Add(Guna2HtmlLabel1)
+        Guna2Panel1.Controls.Add(studname_txtbx)
+        Guna2Panel1.Controls.Add(uid_txtbx)
+        Guna2Panel1.CustomizableEdges = CustomizableEdges9
+        Guna2Panel1.Location = New Point(245, 65)
+        Guna2Panel1.Name = "Guna2Panel1"
+        Guna2Panel1.ShadowDecoration.CustomizableEdges = CustomizableEdges10
+        Guna2Panel1.Size = New Size(523, 498)
+        Guna2Panel1.TabIndex = 0
+        ' 
+        ' Guna2GradientButton2
+        ' 
+        Guna2GradientButton2.CustomizableEdges = CustomizableEdges1
+        Guna2GradientButton2.DisabledState.BorderColor = Color.DarkGray
+        Guna2GradientButton2.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2GradientButton2.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2GradientButton2.DisabledState.FillColor2 = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2GradientButton2.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2GradientButton2.Font = New Font("Segoe UI", 9F)
+        Guna2GradientButton2.ForeColor = Color.White
+        Guna2GradientButton2.Location = New Point(151, 365)
+        Guna2GradientButton2.Name = "Guna2GradientButton2"
+        Guna2GradientButton2.ShadowDecoration.CustomizableEdges = CustomizableEdges2
+        Guna2GradientButton2.Size = New Size(180, 45)
+        Guna2GradientButton2.TabIndex = 6
+        Guna2GradientButton2.Text = "Clear"
+        ' 
+        ' Guna2GradientButton1
+        ' 
+        Guna2GradientButton1.CustomizableEdges = CustomizableEdges3
+        Guna2GradientButton1.DisabledState.BorderColor = Color.DarkGray
+        Guna2GradientButton1.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2GradientButton1.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2GradientButton1.DisabledState.FillColor2 = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2GradientButton1.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2GradientButton1.Font = New Font("Segoe UI", 9F)
+        Guna2GradientButton1.ForeColor = Color.White
+        Guna2GradientButton1.Location = New Point(151, 301)
+        Guna2GradientButton1.Name = "Guna2GradientButton1"
+        Guna2GradientButton1.ShadowDecoration.CustomizableEdges = CustomizableEdges4
+        Guna2GradientButton1.Size = New Size(180, 45)
+        Guna2GradientButton1.TabIndex = 5
+        Guna2GradientButton1.Text = "login"
+        ' 
+        ' Guna2HtmlLabel3
+        ' 
+        Guna2HtmlLabel3.BackColor = Color.Transparent
+        Guna2HtmlLabel3.ForeColor = SystemColors.AppWorkspace
+        Guna2HtmlLabel3.Location = New Point(111, 146)
+        Guna2HtmlLabel3.Name = "Guna2HtmlLabel3"
+        Guna2HtmlLabel3.Size = New Size(35, 17)
+        Guna2HtmlLabel3.TabIndex = 4
+        Guna2HtmlLabel3.Text = "Name"
+        ' 
+        ' Guna2HtmlLabel2
+        ' 
+        Guna2HtmlLabel2.BackColor = Color.Transparent
+        Guna2HtmlLabel2.ForeColor = SystemColors.AppWorkspace
+        Guna2HtmlLabel2.Location = New Point(111, 73)
+        Guna2HtmlLabel2.Name = "Guna2HtmlLabel2"
+        Guna2HtmlLabel2.Size = New Size(22, 17)
+        Guna2HtmlLabel2.TabIndex = 3
+        Guna2HtmlLabel2.Text = "UID"
+        ' 
+        ' Guna2HtmlLabel1
+        ' 
+        Guna2HtmlLabel1.BackColor = Color.Transparent
+        Guna2HtmlLabel1.ForeColor = SystemColors.AppWorkspace
+        Guna2HtmlLabel1.Location = New Point(206, 35)
+        Guna2HtmlLabel1.Name = "Guna2HtmlLabel1"
+        Guna2HtmlLabel1.Size = New Size(33, 17)
+        Guna2HtmlLabel1.TabIndex = 2
+        Guna2HtmlLabel1.Text = "Login"
+        ' 
+        ' studname_txtbx
+        ' 
+        studname_txtbx.CustomizableEdges = CustomizableEdges5
+        studname_txtbx.DefaultText = ""
+        studname_txtbx.DisabledState.BorderColor = Color.FromArgb(CByte(208), CByte(208), CByte(208))
+        studname_txtbx.DisabledState.FillColor = Color.FromArgb(CByte(226), CByte(226), CByte(226))
+        studname_txtbx.DisabledState.ForeColor = Color.FromArgb(CByte(138), CByte(138), CByte(138))
+        studname_txtbx.DisabledState.PlaceholderForeColor = Color.FromArgb(CByte(138), CByte(138), CByte(138))
+        studname_txtbx.FocusedState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        studname_txtbx.Font = New Font("Segoe UI", 9F)
+        studname_txtbx.HoverState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        studname_txtbx.Location = New Point(111, 169)
+        studname_txtbx.Name = "studname_txtbx"
+        studname_txtbx.PasswordChar = ChrW(0)
+        studname_txtbx.PlaceholderText = ""
+        studname_txtbx.SelectedText = ""
+        studname_txtbx.ShadowDecoration.CustomizableEdges = CustomizableEdges6
+        studname_txtbx.Size = New Size(277, 36)
+        studname_txtbx.TabIndex = 1
+        ' 
+        ' uid_txtbx
+        ' 
+        uid_txtbx.CustomizableEdges = CustomizableEdges7
+        uid_txtbx.DefaultText = ""
+        uid_txtbx.DisabledState.BorderColor = Color.FromArgb(CByte(208), CByte(208), CByte(208))
+        uid_txtbx.DisabledState.FillColor = Color.FromArgb(CByte(226), CByte(226), CByte(226))
+        uid_txtbx.DisabledState.ForeColor = Color.FromArgb(CByte(138), CByte(138), CByte(138))
+        uid_txtbx.DisabledState.PlaceholderForeColor = Color.FromArgb(CByte(138), CByte(138), CByte(138))
+        uid_txtbx.FocusedState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        uid_txtbx.Font = New Font("Segoe UI", 9F)
+        uid_txtbx.HoverState.BorderColor = Color.FromArgb(CByte(94), CByte(148), CByte(255))
+        uid_txtbx.Location = New Point(111, 96)
+        uid_txtbx.Name = "uid_txtbx"
+        uid_txtbx.PasswordChar = ChrW(0)
+        uid_txtbx.PlaceholderText = ""
+        uid_txtbx.SelectedText = ""
+        uid_txtbx.ShadowDecoration.CustomizableEdges = CustomizableEdges8
+        uid_txtbx.Size = New Size(277, 36)
+        uid_txtbx.TabIndex = 0
+        ' 
+        ' LOGIN
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1064, 681)
+        Controls.Add(Guna2Panel1)
+        Name = "LOGIN"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Student Management System"
+        Guna2Panel1.ResumeLayout(False)
+        Guna2Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Guna2Panel1 As Guna.UI2.WinForms.Guna2Panel
+    Friend WithEvents Guna2GradientButton2 As Guna.UI2.WinForms.Guna2GradientButton
+    Friend WithEvents Guna2GradientButton1 As Guna.UI2.WinForms.Guna2GradientButton
+    Friend WithEvents Guna2HtmlLabel3 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2HtmlLabel2 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents Guna2HtmlLabel1 As Guna.UI2.WinForms.Guna2HtmlLabel
+    Friend WithEvents studname_txtbx As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents uid_txtbx As Guna.UI2.WinForms.Guna2TextBox
+
+End Class
---
+++ LOGIN.vb
@@ -0,0 +1,64 @@
+﻿Imports Guna.UI2.WinForms
+Imports Microsoft.VisualBasic.Logging
+Imports MySql.Data.MySqlClient
+
+Public Class LOGIN
+    Private Sub Guna2GradientButton2_Click(sender As Object, e As EventArgs) Handles Guna2GradientButton2.Click
+        uid_txtbx.Clear()
+        studname_txtbx.Clear()
+    End Sub
+
+    Private Sub Guna2GradientButton1_Click(sender As Object, e As EventArgs) Handles Guna2GradientButton1.Click
+        Dim idcode As String = uid_txtbx.Text
+        If AuthenticateUser(idcode) Then
+            Dim userRole As String = GetUserRole(idcode)
+            If userRole = "emp" Then
+                con.Close()
+                'display empid in txtbox in login form
+                'SetEmpid()
+                'LOGIN.Hide()
+                Me.Hide()
+                'studpage.Show()
+
+            ElseIf uid_txtbx.Text = "admin123" Then
+                'punta sa admin page    
+                'adminpage.Show()
+            Else
+                MsgBox("Invalid role for this user")
+
+            End If
+        End If
+    End Sub
+
+    Private Function AuthenticateUser(qrcode As String) As Boolean
+        openCon()
+        Dim query As String = "SELECT * FROM logins WHERE qrcode = @qrcode"
+
+        Using cmd As New MySqlCommand(query, con)
+            cmd.Parameters.AddWithValue("@qrcode", qrcode)
+            Try
+                Dim count As Integer = CInt(cmd.ExecuteScalar())
+                Return count > 0
+                con.Close()
+                'SetEmpid()
+
+            Catch ex As Exception
+                MessageBox.Show("Error connecting to the database: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return False
+            End Try
+        End Using
+
+    End Function
+
+    Private Function GetUserRole(qrcode As String) As String
+        Dim query As String = "SELECT roles FROM logins WHERE qrcode = @qrcode"
+        Using command As New MySqlCommand(query, con)
+            command.Parameters.AddWithValue("@qrcode", qrcode)
+            Dim result As Object = command.ExecuteScalar()
+            If result IsNot Nothing Then
+                Return result.ToString()
+            End If
+        End Using
+        Return String.Empty
+    End Function
+End Class
