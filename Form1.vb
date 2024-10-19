efef9708b1f8919df72f62ac7938411d218b72c2
Magnaye16 <151270874+Magnaye16@users.noreply.github.com>
2024-10-19 16:21:55 +0800

    adjujst / add

--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,3 @@
 ﻿Public Class Form1
-    Private Sub Guna2TextBox4_TextChanged(sender As Object, e As EventArgs) Handles Guna2TextBox4.TextChanged
 
-    End Sub
-
-    Private Sub Guna2CirclePictureBox1_Click(sender As Object, e As EventArgs) Handles Guna2CirclePictureBox1.Click
-
-    End Sub
 End Class
