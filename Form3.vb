ac440dc512f644f324dc91a550c7e9082095750c
KaiYue25 <151806079+KaiYue25@users.noreply.github.com>
2024-10-24 20:15:57 +0800

    2

--- Form3.vb
+++ Form3.vb
@@ -47,8 +47,7 @@ Public Class Form3
     End Sub
 
     Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
-        Clear()
-
+        'Clear()
     End Sub
 
     Private Sub Guna2Button4_Click(sender As Object, e As EventArgs) Handles Guna2Button4.Click
