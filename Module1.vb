b637e6970d410bbdac7ca49e497817bebe533c67
Magnaye16 <151270874+Magnaye16@users.noreply.github.com>
2024-10-29 06:50:38 +0800

    LIPAT MUNA LOCAHHOST

--- Module1.vb
+++ Module1.vb
@@ -4,7 +4,8 @@ Module Module1
     Public cmd As New MySqlCommand
 
     Sub openCon()
-        con.ConnectionString = "server=100.24.17.9; username=root; password=Alex0987654321!; database=SMS_DB"
+        'con.ConnectionString = "server=100.24.17.9; username=root; password=Alex0987654321!; database=SMS_DB"
+        con.ConnectionString = "Localhost; username=root; password=; database=SMS_DB"
         con.Open()
 
     End Sub
